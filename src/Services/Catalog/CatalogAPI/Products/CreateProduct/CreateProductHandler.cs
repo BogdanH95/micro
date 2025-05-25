e8872126a6204ae195b053e1fffd5223d2483415
BogdanH95 <bogdan.halmaghi@gmail.com>
2025-05-25 22:20:38 +0300

    Add Building Blocks and CQRS abstraction

--- src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -1,10 +1,18 @@
-﻿namespace CatalogAPI.Products.CreateProduct
+﻿using BuildingBlocks.CQRS;
+using MediatR;
+
+namespace CatalogAPI.Products.CreateProduct
 {
+    public record CreateProductCommand(string Name, List<string> Categories, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
 
-    public record CreateProductCommand
-
-    public class CreateProductHandler
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            //Business logic to create a product
+            throw new NotImplementedException();
+        }
     }
 }
